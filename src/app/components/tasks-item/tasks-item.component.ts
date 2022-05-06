16430c55c162efbbf49ab893bca929b18ecbaec4
facundo <brizuelafacundoignacio@gmail.com>
2022-05-06 01:44:44 -0300

    componennte de lista de items

---
+++ src/app/components/tasks-item/tasks-item.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Task } from '../../Task'
+import { TASKS } from '../../mock-tasks'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+
+@Component({
+  selector: 'app-tasks-item',
+  templateUrl: './tasks-item.component.html',
+  styleUrls: ['./tasks-item.component.css']
+})
+export class TasksItemComponent implements OnInit {
+
+  @Input() task: Task = TASKS[0];
+  faTimes = faTimes;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
